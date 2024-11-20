0f138612e1f0e12486602f0c597edad1583fc235
loganepps <logepps@gmail.com>
2024-11-19 18:19:31 -0600

    completed task 3

--- queries.sql
+++ queries.sql
@@ -1,15 +1,10 @@
 --Part 1
---SQL command to read our table data:
---SELECT *
---FROM techjobs;
---
---id INT,
---employer VARCHAR(255),
---name VARCHAR(255),
---skills VARCHAR(255)
+SELECT * FROM techjobs;
 
 --Part 2
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
+DROP TABLE job;
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,13 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
     @NotBlank
     @NotNull
     @Size(min = 3, max = 100, message = "Location name must be between 3 and 100 characters")
